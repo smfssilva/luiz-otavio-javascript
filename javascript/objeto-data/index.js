43ed87ba7301263961fed43d7de072e4233c670e
smfssilva <smfssilva@yahoo.com>
2021-05-10 23:32:45 -0300

    Objeto Date

---
+++ javascript/objeto-data/index.js
@@ -0,0 +1,11 @@
+const data = new Date();
+console.log(data.toLocaleDateString());
+
+console.log(data.getDate());
+console.log(data.getMonth());
+console.log(data.getFullYear());
+console.log(data.getHours());
+console.log(data.getMinutes());
+console.log(data.getSeconds());
+console.log(data.getDay());
+console.log(`${data.getDate()}/${data.getMonth()}/${data.getFullYear()}`);
