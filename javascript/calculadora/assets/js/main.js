b1a160f4a048c520f645c658c78261d37553d27d
smfssilva <smfssilva@yahoo.com>
2021-05-11 00:35:15 -0300

    Calculadora

---
+++ javascript/calculadora/assets/js/main.js
@@ -0,0 +1,74 @@
+function criaCalculadora() {
+    return {
+
+        display: document.querySelector('.display'),
+        btnClear: document.querySelector('.btn-clear'),
+
+        iniciar() {
+            this.clickBototes();
+            this.pressionaEnter();
+        },
+
+        pressionaEnter() {
+            this.display.addEventListener('keyup', (e) => {
+                if (e.keucode === 13) {
+                    this.realizaConta();
+                }
+            })
+        }
+
+        clearDisplay() {
+            this.display.value = '';
+        },
+
+        apagaUm() {
+            this.display.value = this.display.value.slice(0, -1)
+        },
+
+
+        realizaConta() {
+            let conta = this.display.value;
+            try {
+                conta = eval(conta);
+                if (!conta) {
+                    alert('Conta inválida')
+                    return
+                }
+
+                this.display.value = conta;
+
+            } catch (e) {
+                alert('Conta inválida')
+                return
+            }
+        },
+
+        clickBototes() {
+            document.addEventListener('click', (e) => {
+                const el = e.target;
+                if (el.classList.contains('btn-num')) {
+                    this.btnParaDisplay(el.innerText);
+                }
+
+                if (el.classList.contains('btn-clear')) {
+                    this.clearDisplay();
+                }
+
+                if (el.classList.contains('btn-del')) {
+                    this.apagaUm();
+                }
+
+                if (el.classList.contains('btn-eq')) {
+                    this.realizaConta();
+                }
+            })
+        },
+
+        btnParaDisplay(valor) {
+            this.display.value += valor;
+        }
+    }
+}
+
+const calculadora = criaCalculadora();
+calculadora.iniciar();
